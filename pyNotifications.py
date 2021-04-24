e47d90ef8f52ccc0e3fe63ef6e43c9fc7f677240
Shawn <shawn.jacobsen0@gmail.com>
2021-04-24 18:49:45 -0400

    adding endpoint to post sleep data

---
+++ pyNotifications.py
@@ -0,0 +1,19 @@
+import smtplib, ssl
+
+def sendEmail(port, smtp_server, sender_email, sender_psw, receiver_email, text):
+
+  rval = True # true if success, false if not
+  try:
+    assembled_message = f"From: {sender_email}\r\nTo: {receiver_email}\r\nSubject: {''}\r\n\r\n{text}"
+    server = smtplib.SMTP(smtp_server,port)
+    CONTEXT = ssl.create_default_context()
+    server.starttls(context=CONTEXT) # Secure the connection
+    server.login(sender_email, sender_psw)
+    server.sendmail(sender_email, receiver_email, assembled_message)
+  except Exception as err:
+    print("ERROR: Cannot send email")
+    print(err)
+    rval = False
+
+  return rval
+  
--- server.py
+++ server.py
@@ -1,4 +1,26 @@
 from flask import Flask, request
+import json
+
+from pyNotifications import (
+  sendEmail
+)
+
+with open('email_secret.json') as f:
+  EMAIL_DATA = json.load(f)
+
+# Notification data for sending text message
+PORT = 587  # For starttls
+SMTP_SERVER = "smtp.gmail.com"
+SENDER_EMAIL = EMAIL_DATA['email']
+SENDER_PSW = EMAIL_DATA['password']
+# RECEIVER_EMAIL = "6143701557@messaging.sprintpcs.com"
+RECEIVER_EMAIL = "shawn.jacobsen0@gmail.com"
+
+# sendEmail(PORT, SMTP_SERVER, SENDER_EMAIL, SENDER_PSW, RECEIVER_EMAIL, text)
+
+# read in email json data
+with open('email_secret.json') as f:
+    EMAIL_DATA = json.load(f)
 
 app = Flask(__name__)
 
@@ -8,8 +30,10 @@ def welcome():
 
 @app.route('/api/sleep-data', methods=['POST'])
 def print_data():
-    print(request.form)
-    return "works"
+    text = f"values:\r\n{request.values}\r\n\r\nForm:\r\n{request.form}\r\n\r\nJSON:\r\n{request.json}"
+    print(text)
+    sendEmail(PORT, SMTP_SERVER, SENDER_EMAIL, SENDER_PSW, RECEIVER_EMAIL, text)
+    return "Received!"
 
 if __name__ == "__main__":
     app.run(debug=True)
