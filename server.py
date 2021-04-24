7c74a9a70f9fdadc7fc67ad92c1053ec39846bc8
Shawn <shawn.jacobsen0@gmail.com>
2021-04-24 19:52:33 -0400

    testing

--- server.py
+++ server.py
@@ -6,16 +6,16 @@ from pyNotifications import (
 )
 
 # read in email json data
-with open('email_secret.json') as f:
-    EMAIL_DATA = json.load(f)
+#with open('email_secret.json') as f:
+#    EMAIL_DATA = json.load(f)
 
 # Notification data for sending text message
-PORT = 587  # For starttls
-SMTP_SERVER = "smtp.gmail.com"
-SENDER_EMAIL = EMAIL_DATA['email']
-SENDER_PSW = EMAIL_DATA['password']
+#PORT = 587  # For starttls
+#SMTP_SERVER = "smtp.gmail.com"
+#SENDER_EMAIL = EMAIL_DATA['email']
+#SENDER_PSW = EMAIL_DATA['password']
 # RECEIVER_EMAIL = "6143701557@messaging.sprintpcs.com"
-RECEIVER_EMAIL = "shawn.jacobsen0@gmail.com"
+#RECEIVER_EMAIL = "shawn.jacobsen0@gmail.com"
 
 # sendEmail(PORT, SMTP_SERVER, SENDER_EMAIL, SENDER_PSW, RECEIVER_EMAIL, text)
 
