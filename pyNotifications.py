ba6f6b72b4a0ddd38cade4c270a3b8b14a2960a2
Shawn <shawn.jacobsen0@gmail.com>
2021-04-24 21:00:51 -0400

    changing dependency to flask_mail

--- pyNotifications.py
+++ pyNotifications.py
@@ -1,14 +1,48 @@
-import smtplib, ssl
+import os
+import json
+from flask import Flask
+from flask_mail import Mail, Message
 
-def sendEmail(port, smtp_server, sender_email, sender_psw, receiver_email, text):
+# RECEIVER_EMAIL = "6143701557@messaging.sprintpcs.com"
+RECEIVER_EMAIL = "shawn.jacobsen0@gmail.com"
+SENDER_EMAIL = ""
+SENDER_PSW = ""
+# get env vars
+if os.environ.get('IS_PROD', None):
+    SENDER_EMAIL = os.environ.get('SENDER_EMAIL')
+    SENDER_PSW = os.environ.get('SENDER_PSW')
+else:
+    print("\nWelcome to DEVELOPMENT MODE....")
+    # read in email json data
+    with open('email_secret.json') as f:
+      EMAIL_DATA = json.load(f)
+    SENDER_EMAIL = EMAIL_DATA['email']
+    SENDER_PSW = EMAIL_DATA['password']
 
+def sendEmail(app, text):
   rval = True # true if success, false if not
-  try:
-    assembled_message = f"From: {sender_email}\r\nTo: {receiver_email}\r\nSubject: {''}\r\n\r\n{text}"
-    server = smtplib.SMTP(smtp_server,port)
-    server.starttls()
-    server.login(sender_email, sender_psw)
-    server.sendmail(sender_email, receiver_email, assembled_message)
+
+  # setup app for email send
+  app.config.update(dict(
+    MAIL_SERVER = 'smtp.gmail.com',
+    MAIL_PORT = 587,
+    MAIL_USE_TLS = True,
+    MAIL_USE_SSL = False,
+    MAIL_USERNAME = SENDER_EMAIL,
+    MAIL_PASSWORD = SENDER_PSW,
+  ))
+
+  mail = Mail(app)
+  msg = Message(
+    '',
+    sender = SENDER_EMAIL,
+    recipients = [RECEIVER_EMAIL]
+  )
+  msg.body = text
+
+  try:   
+    with app.app_context():
+      mail.send(msg)
   except Exception as err:
     print("ERROR: Cannot send email")
     print(err)
--- server.py
+++ server.py
@@ -6,40 +6,18 @@ from pyNotifications import (
   sendEmail
 )
 
-SENDER_EMAIL = ""
-SENDER_PSW = ""
-# get env vars
-if os.environ.get('IS_PROD', None):
-    SENDER_EMAIL = os.environ.get('SENDER_EMAIL')
-    SENDER_PSW = os.environ.get('SENDER_PSW')
-else:
-    print("\nWelcome to DEVELOPMENT MODE....")
-    # read in email json data
-    with open('email_secret.json') as f:
-        EMAIL_DATA = json.load(f)
-    SENDER_EMAIL = EMAIL_DATA['email']
-    SENDER_PSW = EMAIL_DATA['password']
-
-# Notification data for sending text message
-PORT = 587  # For starttls
-SMTP_SERVER = "smtp.gmail.com"
-# RECEIVER_EMAIL = "6143701557@messaging.sprintpcs.com"
-RECEIVER_EMAIL = "shawn.jacobsen0@gmail.com"
-
-# sendEmail(PORT, SMTP_SERVER, SENDER_EMAIL, SENDER_PSW, RECEIVER_EMAIL, text)
-
 app = Flask(__name__)
 
 @app.route('/api/')
 def welcome():
-    rval = f"Welcome to Shawn's personal API!\r\nSENDER_EMAIL: {SENDER_EMAIL}\r\nSENDER_PSW: {SENDER_PSW}"
+    rval = f"Welcome to Shawn's personal API!"
     return rval
 
 @app.route('/api/sleep-data', methods=['POST'])
 def print_data():
     text = f"values:\r\n{request.values}\r\n\r\nForm:\r\n{request.form}\r\n\r\nJSON:\r\n{request.json}"
     print(text)
-    rval = sendEmail(PORT, SMTP_SERVER, SENDER_EMAIL, SENDER_PSW, RECEIVER_EMAIL, text)
+    rval = sendEmail(app, text)
     return f"{'Success!' if rval else 'something went wrong...'}"
 
 if __name__ == "__main__":
